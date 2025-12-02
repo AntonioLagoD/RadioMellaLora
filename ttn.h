24abde69ac892267e0fabe0ad4467b3ace4ce0fe
AntonioLagoD <44477003+AntonioLagoD@users.noreply.github.com>
2025-12-02 11:00:06 +0100

    Update ttn.h

--- ttn.h
+++ ttn.h
@@ -1,4 +1,10 @@
 const LoRaWANBand_t Region = EU868;
+// OTAA
+uint64_t joinEUI = 0x0000000000000000;
+uint64_t devEUI = 0;
+uint8_t appKey[] = {  };
+uint8_t nwkKey[] = {  };
+
 // CLAVES ABP 
 uint32_t devAddr =        0xFFFFFFFF;
 uint8_t fNwkSIntKey[] = {  };
@@ -6,3 +12,4 @@ uint8_t sNwkSIntKey[] = {  };
 uint8_t nwkSEncKey[] =  {  };
 
 uint8_t appSKey[] =     {  };
+
